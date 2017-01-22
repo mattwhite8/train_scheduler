3fadf56eb0bbc8deab7a37fb558043fbffd3d7b0
Matt White <mattwhite1337@gmail.com>
2017-01-22 11:11:21 -0500

    revamped app.js to do all work without using object constructor

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,58 +12,44 @@ var database = firebase.database();
 
 var trainsRef = database.ref('trains/');
 
-var objectArr = [];
-
 var timeOut;
 
-function trainRow(train, destination, initialTime, frequency){
-	this.train = train;
-	this.destination = destination;
-	this.initialTime = moment(initialTime, 'h:mm a');
-	this.frequency = frequency;
-}
+function domSet(train){
 
-function domSet(array){
+	var now = moment();
+	var arrivalTime = moment(train.time, 'h:mm a');
 
-	$("#table-body").empty();
-
-	console.log('domSet was called');
-
-	for(var i = 0; i < array.length; i++){
-		var now = moment();
-
-		while(array[i].initialTime.isBefore(now, 'minute') === true){
-			array[i].initialTime.add(array[i].frequency, 'm');
-		}
-
-		var button = $("<button>").text('delete');
-
-		//Scope work around: pass your extra data in as below:
-		button.on('click', { extra : array[i], index : i }, function(event){
-			var data = event.data;
-			console.log(data.extra.train);	
-
-			trainsRef.child(data.extra.train).remove();
-			$("tbody").children("tr").eq(data.index).empty();
-
-			if(timeOut){
-				clearInterval(timeOut);
-				timeOut = setInterval(function(){domSet(objectArr)}, 60000);
-			} else{
-				timeOut = setInterval(function(){domSet(objectArr)}, 60000);		
-			}	
-					
-		})
-
-		$("#table-body").append("<tr><td>"+array[i].train+"</td>"+
-			"<td>"+array[i].destination+"</td>"+
-			"<td>"+array[i].frequency+"</td>"+
-			"<td>"+array[i].initialTime.format('h:mm a')+"</td>"+
-			"<td>"+"Approximately "+now.to(array[i].initialTime)+"</td>"+
-			"<td></td></tr>");			
-
-		$("#table-body").children().eq(i).children().last().append(button);
+	while(arrivalTime.isBefore(now, 'minute') === true){
+		arrivalTime.add(train.frequency, 'm');
 	}
+
+	var button = $("<button>").text('delete');
+
+	//Scope work around: pass your extra data in as below:
+	button.on('click', { extra : train }, function(event){
+		var data = event.data;
+		console.log(data.extra.train);	
+
+		trainsRef.child(data.extra.train).remove();
+		$(this).closest('tr').remove();
+
+		if(timeOut){
+			clearInterval(timeOut);
+			timeOut = setInterval(function(){domSet(data.extra.train)}, 60000);
+		} else{
+			timeOut = setInterval(function(){domSet(data.extra.train)}, 60000);		
+		}	
+				
+	})
+
+	$("#table-body").append("<tr><td>"+train.train+"</td>"+
+		"<td>"+train.destination+"</td>"+
+		"<td>"+train.frequency+"</td>"+
+		"<td>"+train.time+"</td>"+
+		"<td>"+"Approximately "+now.to(moment(train.time,'h:mm a'))+"</td>"+
+		"<td></td></tr>");			
+
+	$("#table-body").children('tr').children('td').last().append(button);
 		
 }
 
@@ -85,20 +71,8 @@ $("#train-submit").on('click', function(event){
 		train: trainName,
 		destination: trainDestination,
 		frequency: freq,
-		time: firstTime
+		time: moment(firstTime, 'h:mm a').format('h:mm a')
 	});
-
-	// $("#table-body").html("<img src='assets/images/loader.gif'>");
-
-	//make sure the data gets to firebase first
-	setTimeout(function(){domSet(objectArr)}, 2000);
-
-	if(timeOut){
-		clearInterval(timeOut);
-		timeOut = setInterval(function(){domSet(objectArr)}, 60000);
-	} else{
-		timeOut = setInterval(function(){domSet(objectArr)}, 60000);		
-	}
 
 });
 
@@ -106,27 +80,13 @@ trainsRef.on("child_added", function(snapshot){
 	var train = snapshot.val();
 	console.log(train);
 
-	objectArr.push(new trainRow(train.train, train.destination, train.time, train.frequency));
+	domSet(train);
 
 });
 
-trainsRef.on("child_removed", function(snapshot){
-	if(objectArr.length === 1){
-		objectArr = [];
-	}
-	for(var i = 0; i < objectArr.length; i++){
-		if(objectArr[i].train === snapshot.val().train){
-			objectArr = objectArr.splice(objectArr[i], 1);
-		}
-	}
-})
 
-$( document ).ready(function(){
 
-	//wait for child_added events
-	setTimeout(function(){domSet(objectArr)}, 2000);
 
-})
 
 
 
