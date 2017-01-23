214a30b581588d2dc574f706daf93c5d507807f3
Matt White <mattwhite1337@gmail.com>
2017-01-22 20:02:52 -0500

    adding github auth

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,8 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var provider = new firebase.auth.GithubAuthProvider();
+
 var database = firebase.database();
 
 var trainsRef = database.ref('trains/');
@@ -19,6 +21,34 @@ var timeOut;
 var trainKeys;
 
 var trainData;
+
+function githubSignin() {
+   firebase.auth().signInWithPopup(provider)
+   
+   .then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+		
+      console.log(token)
+      console.log(user)
+   }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+		
+      console.log(error.code)
+      console.log(error.message)
+   });
+}
+
+function githubSignout(){
+   firebase.auth().signOut()
+   
+   .then(function() {
+      console.log('Signout successful!')
+   }, function(error) {
+      console.log('Signout failed')
+   });
+}
 
 function domSet(data, keys){
 
